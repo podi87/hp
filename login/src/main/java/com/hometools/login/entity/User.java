a94463886d448c49f971945664f667ba1a40f9e5
András Podobni <podobni.andras@gmail.com>
2019-01-29 22:06:45 +0100

    add User entity and repository

---
+++ login/src/main/java/com/hometools/login/entity/User.java
@@ -0,0 +1,20 @@
+package com.hometools.login.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    private String name;
+    private String password;
+
+}
