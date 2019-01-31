4a3b87397f11f524ef548de9bee3454b0225b176
András Podobni <podobni.andras@gmail.com>
2019-01-31 18:40:27 +0100

    making authorization server;

--- login/src/main/java/com/hometools/login/configuration/WebConfig.java
+++ login/src/main/java/com/hometools/login/configuration/WebConfig.java
@@ -6,12 +6,15 @@ import java.util.List;
 import javax.servlet.Filter;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
@@ -29,38 +32,27 @@ import org.springframework.web.filter.CompositeFilter;
 @Configuration
 @EnableOAuth2Client
 @EnableAuthorizationServer
+@Order(200)
 public class WebConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     OAuth2ClientContext oauth2ClientContext;
 
     @Bean
-    @ConfigurationProperties("facebook.client")
-    public AuthorizationCodeResourceDetails facebook() {
-        return new AuthorizationCodeResourceDetails();
+    @ConfigurationProperties("github")
+    public ClientResource github() {
+        return new ClientResource();
     }
 
     @Bean
-    @ConfigurationProperties("facebook.resource")
-    public ResourceServerProperties facebookResource() {
-        return new ResourceServerProperties();
-    }
-
-    @Bean
-    @ConfigurationProperties("github.client")
-    public AuthorizationCodeResourceDetails github() {
-        return new AuthorizationCodeResourceDetails();
-    }
-
-    @Bean
-    @ConfigurationProperties("github.resource")
-    public ResourceServerProperties githubResource() {
-        return new ResourceServerProperties();
+    @ConfigurationProperties("facebook")
+    public ClientResource facebook() {
+        return new ClientResource();
     }
 
     @Bean
     public FilterRegistrationBean<OAuth2ClientContextFilter> oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
-        FilterRegistrationBean<OAuth2ClientContextFilter> registration = new FilterRegistrationBean<OAuth2ClientContextFilter>();
+        FilterRegistrationBean<OAuth2ClientContextFilter> registration = new FilterRegistrationBean<>();
         registration.setFilter(filter);
         registration.setOrder(-100);
         return registration;
@@ -72,8 +64,7 @@ public class WebConfig extends WebSecurityConfigurerAdapter {
                 .antMatcher("/**").authorizeRequests()
                 .antMatchers("/", "/login**", "/error**").permitAll()
                 .anyRequest().authenticated()
-                .and().exceptionHandling()
-                .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/"))
+                .and().exceptionHandling().authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/"))
                 .and().logout().logoutSuccessUrl("/").permitAll()
                 .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 .and().addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
@@ -82,35 +73,39 @@ public class WebConfig extends WebSecurityConfigurerAdapter {
     private Filter ssoFilter() {
         CompositeFilter filter = new CompositeFilter();
         List<Filter> filters = new ArrayList<>();
-        filters.add(ssoFilter(getFilter("/login/facebook"),
-                getTemplate(facebook()),
-                getTokenServices(facebookResource(), facebook())));
-        filters.add(ssoFilter(getFilter("/login/github"),
-                getTemplate(github()),
-                getTokenServices(githubResource(), github())));
+        filters.add(ssoFilter(facebook(),"/login/facebook"));
+        filters.add(ssoFilter(github(),"/login/github"));
         filter.setFilters(filters);
         return filter;
     }
 
-    private Filter ssoFilter(OAuth2ClientAuthenticationProcessingFilter filter,
-                             OAuth2RestTemplate template,
-                             UserInfoTokenServices tokenServices) {
+    private Filter ssoFilter(ClientResource client, String path) {
+        OAuth2ClientAuthenticationProcessingFilter filter = new OAuth2ClientAuthenticationProcessingFilter(path);
+        OAuth2RestTemplate template = new OAuth2RestTemplate(client.getClient(), oauth2ClientContext);
         filter.setRestTemplate(template);
+        UserInfoTokenServices tokenServices = new UserInfoTokenServices(
+                client.getResource().getUserInfoUri(), client.getClient().getClientId());
         tokenServices.setRestTemplate(template);
         filter.setTokenServices(tokenServices);
         return filter;
     }
 
-    private UserInfoTokenServices getTokenServices(final ResourceServerProperties resourceServerProperties, final AuthorizationCodeResourceDetails facebook) {
-        return new UserInfoTokenServices(resourceServerProperties.getUserInfoUri(), facebook.getClientId());
-    }
+    private class ClientResource {
 
-    private OAuth2RestTemplate getTemplate(final AuthorizationCodeResourceDetails facebook) {
-        return new OAuth2RestTemplate(facebook, oauth2ClientContext);
-    }
+        @NestedConfigurationProperty
+        private AuthorizationCodeResourceDetails client = new AuthorizationCodeResourceDetails();
 
-    private OAuth2ClientAuthenticationProcessingFilter getFilter(final String s) {
-        return new OAuth2ClientAuthenticationProcessingFilter(s);
+        @NestedConfigurationProperty
+        private ResourceServerProperties resource = new ResourceServerProperties();
+
+        public AuthorizationCodeResourceDetails getClient() {
+            return client;
+        }
+
+        public ResourceServerProperties getResource() {
+            return resource;
+        }
+
     }
 
 }
--- login/src/main/java/com/hometools/login/controller/RestCtrl.java
+++ login/src/main/java/com/hometools/login/controller/RestCtrl.java
@@ -1,5 +1,14 @@
 package com.hometools.login.controller;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.Principal;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +18,15 @@ public class RestCtrl {
     @RequestMapping("/hello")
     public String test() {
         return "Hello World!";
+    }
+
+    @GetMapping({"/user", "/me"})
+    public Map<String, String> user(final Principal principal) {
+        Map<String, String> modelMap = new LinkedHashMap<>();
+//        if (!(auth instanceof AnonymousAuthenticationToken) && (auth instanceof UsernamePasswordAuthenticationToken)){
+//            UsernamePasswordAuthenticationToken upat = (UsernamePasswordAuthenticationToken) auth;
+        modelMap.put("name", principal.getName());
+        return modelMap;
     }
 
 }
